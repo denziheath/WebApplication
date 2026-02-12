06a97d932ac58f26f8b5d5b72d65a9b8a7d3b973
denziheath <denziheath@yahoo.com>
2026-02-12 12:13:44 -0700

    changes

---
+++ WebApplication1/TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Playwright;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var requestContext = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+            {
+                BaseURL = "https://localhost:7106"
+            });
+
+            requestContext.
+        }
+    }
+}
